a510f87c775a3f4bd569718f66464159e6e6d5c1
raunak <raunakbiswas05@gmail.com>
2025-05-17 00:18:47 +0530

    blank identifier

--- bland_identifiers/bland_identifier.go
+++ bland_identifiers/bland_identifier.go
@@ -5,6 +5,10 @@ func main(){
 	var a,b = return_nums()
 	fmt.Println(a,b)
 
+	//_ is bland identifier and is used when a value isnt used
+	c,_ := return_nums()
+	_=c
+
 }
 
 func return_nums () (int, int){
