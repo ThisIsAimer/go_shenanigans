3fc6e81fbd4121c0c451b59c21033f96c981fe4e
raunak <raunakbiswas05@gmail.com>
2025-05-17 11:15:38 +0530

    different pointer initialisation method

--- pointers_in_arrays/pointers_in_arrays.go
+++ pointers_in_arrays/pointers_in_arrays.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var array [3]int = [3]int{2, 3, 4}
-	var copied_array *[3]int = &array
+	var array  = [3]int{2, 3, 4}
+	var copied_array  = &array
 	copied_array[1] = 10
 
 	fmt.Println(*copied_array)//as copied_array is a pointer array we need to dereference it
