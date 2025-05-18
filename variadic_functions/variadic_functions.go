9591c68b5f4a3c221cfb224281784f3210f102fa
raunak <raunakbiswas05@gmail.com>
2025-05-18 18:39:53 +0530

    variadic functions

---
+++ variadic_functions/variadic_functions.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main(){
+
+	//var numbers = []int{1,2,3,4,5,6,7}
+	var numbers = [8]int{1,2,3,4,5,6,7,8}
+
+	//fmt.Println("sum of nums are", sum(numbers...))
+	fmt.Println("sum of nums are", sum(numbers[:]...))
+	fmt.Println("sum of 5,6,7 are", sum(5,6,7))
+
+}
+
+//variadic parameters must be the last parameter of a function
+func sum(nums ...int) (int){
+	var i int = 0
+
+	for _, v := range nums{
+		i += v
+	}
+
+	return i
+}
