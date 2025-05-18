5fd34e258b75e96b9045785194683830b6eb1b3f
raunak <raunakbiswas05@gmail.com>
2025-05-18 18:42:40 +0530

    array functions

--- variadic_functions/variadic_functions.go
+++ variadic_functions/variadic_functions.go
@@ -7,16 +7,19 @@ func main(){
 	var numbers = [8]int{1,2,3,4,5,6,7,8}
 
 	//fmt.Println("sum of nums are", sum(numbers...))
-	fmt.Println("sum of nums are", sum(numbers[:]...))
-	fmt.Println("sum of 5,6,7 are", sum(5,6,7))
+	// fmt.Println("sum of nums are", sum(numbers[:]...))
+	// fmt.Println("sum of 5,6,7 are", sum(5,6,7))
+	 fmt.Println("sum of nums are", sum(numbers))
 
 }
 
 //variadic parameters must be the last parameter of a function
-func sum(nums ...int) (int){
+// 
+
+func sum(nums [8]int) int {
 	var i int = 0
 
-	for _, v := range nums{
+	for _, v := range nums {
 		i += v
 	}
 
