453472c330e95fe8e33a3ba9730c6d0ef9d0e465
raunak <raunakbiswas05@gmail.com>
2025-05-20 00:16:17 +0530

    recursion in go

--- intermediate/recursion/recursion.go
+++ intermediate/recursion/recursion.go
@@ -1,7 +1,41 @@
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	fmt.Println("factorial is:", factorial(5))
+	fmt.Println("factorial is:", factorial(10))
 
+	fmt.Println("factorial loop is:", factorialLoop(5))
+
+	fmt.Println("sumof figits is:", sumOfDigits(123))
+
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	} else {
+		return n * factorial(n-1)
+	}
+}
+
+func sumOfDigits(num int) int{
+	if num < 10{
+		return num
+	}else{
+		return num%10 + sumOfDigits(num / 10)
+	}
+}
+
+// in loop
+func factorialLoop(n int) int {
+	var i int = 1
+
+	for n > 0 {
+		i *= n
+		n--
+	}
+
+	return i
 }
