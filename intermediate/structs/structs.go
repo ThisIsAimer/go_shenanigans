94dc3f25f949f4ad07b621a924b568738a6e915b
raunak <raunakbiswas05@gmail.com>
2025-05-22 02:40:05 +0530

    ananomous structs in go

--- intermediate/structs/structs.go
+++ intermediate/structs/structs.go
@@ -4,28 +4,48 @@ import (
 	"fmt"
 )
 
-type person struct{
+type Person struct{
 		firstName string;
 		lastName string;
 		age int;
+		address Address;
+		Phone
 
 	}
+
+type Phone struct{
+	number int
+}
+
+type Address struct{
+	city string
+	country string
+}
 
 func main(){
 
-	var person1 = person{
-		firstName: "rudra",
-		lastName: "shiva",
-		age: 12,
+	var person1 = Person{
+		firstName: "Rudra",
+		lastName: "Shiva",
+		age: 14,
+		address: Address{
+			city: "Uttarakhand",
+			country: "Bharat",
+		},
+		Phone: Phone{
+			number: 23464646,
+		},
 	}
 
-	person2 := person{
-		firstName: "gothum",
+	person2 := Person{
+		firstName: "Gothum",
 		age: 19,
 	}
 
 	fmt.Println(person1)
 	fmt.Println(person1.firstName)
+	fmt.Println(person1.address.city)
+	person2.lastName = "Aanand"
 	fmt.Println(person2)
 	fmt.Println("----------------------------------------------------------------")
 
@@ -43,10 +63,21 @@ func main(){
 
 	fmt.Println(user)
 
+	//mutable struct with pointer methods
+	fmt.Println("person age before",person1.age)
+	person1.birthday()
+	fmt.Println("person age after",person1.age)
+
+	fmt.Println(person1.number)//ananomous sturucts can be used like this
+
+
 }
 
 //methods
 
-func (p person) fullName() string{
+func (p Person) fullName() string{
 	return fmt.Sprintln(p.firstName,p.lastName)
+}
+func (p *Person) birthday() {
+	p.age++
 }
