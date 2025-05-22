43b476dd0cbc65db6dc53cd838feb5af1dfacb4e
raunak <raunakbiswas05@gmail.com>
2025-05-22 22:26:22 +0530

    interfaces

---
+++ intermediate/interfaces/interfaces.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+type Geomerty interface{
+	area() float64
+	perim() float64
+}
+
+type Rect struct{
+	hight, width float64
+}
+
+type Circle struct{
+	radius float64
+}
+
+
+func (r Rect) area() float64{
+	return r.hight * r.width
+}
+
+func (c Circle) area() float64{
+	return math.Pi * c.radius *c.radius
+}
+
+func (r Rect) perim() float64{
+	return 2*( r.hight + r.width)
+}
+
+func (c Circle) perim() float64{
+	return 2*math.Pi*c.radius
+}
+
+func (c Circle) diametre() float64{
+	return 2*c.radius
+}
+
+func measure(g Geomerty) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main(){
+
+	var rectangle = Rect{hight: 10,width: 15}
+	var circle = Circle{radius: 10}
+	measure(rectangle)
+	measure(circle)
+
+}
