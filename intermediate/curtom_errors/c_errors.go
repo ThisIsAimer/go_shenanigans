c413f611230d86f2226cf1cba0158acd011373f3
raunak <raunakbiswas05@gmail.com>
2025-05-24 13:22:05 +0530

    error type in structs

--- intermediate/curtom_errors/c_errors.go
+++ intermediate/curtom_errors/c_errors.go
@@ -1,17 +1,27 @@
 package main
-import "fmt"
+
+import (
+	"errors"
+	"fmt"
+)
 
 type customError struct{
 	number int
 	message string
+	er error
 }
 
 func (c *customError) Error() string{
-	return fmt.Sprintf("Error: %d, %s", c.number,c.message)
+	return fmt.Sprintf("Error: %d, %s, %v", c.number,c.message,c.er)
+}
+
+func errFunc() error{
+	return errors.New("new error")
 }
 
 func do_something() error{
-	return &customError{number: 500,message: "something went wrong"}
+	var err = errFunc()
+	return &customError{number: 500,message: "something went wrong",er: err}
 }
 
 
