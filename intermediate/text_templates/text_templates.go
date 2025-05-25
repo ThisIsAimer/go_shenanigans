cf9bd40d1d2a8a6aba52b807852196af88f2be0e
raunak <raunakbiswas05@gmail.com>
2025-05-25 12:27:30 +0530

    more template logic

--- intermediate/text_templates/text_templates.go
+++ intermediate/text_templates/text_templates.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 	//makes template or throws error if template wrong
-	var tmpl, err = template.New("example").Parse("welcome {{.name}}! how are you doing")
+	var tmpl, err = template.New("example").Parse("welcome {{.name}}! how are you doing there are {{.number}} people preasent.\n")
+	//var tmpl = template.Must(template.New("example").Parse("welcome {{.name}}! how are you doing there are {{.number}} people preasent.\n"))
 	if err != nil {
 		panic(err)
 	}
 
 	data := map[string]interface{}{
 		"name": "bro",
+		"number": 100,
 		//"name": 1,
 	}
 
