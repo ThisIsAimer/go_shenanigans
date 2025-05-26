a3f102a27341ee53123f53dd622f4fa91a65b097
raunak <raunakbiswas05@gmail.com>
2025-05-26 15:19:55 +0530

    time location, rounding, truncate and subtracting

--- intermediate/time/time.go
+++ intermediate/time/time.go
@@ -8,6 +8,7 @@ import (
 func main(){
 	fmt.Println(time.Now())
 
+	//custom time
 	istLocation, err := time.LoadLocation("Asia/Kolkata")
 	if err != nil {
 		panic(err)
@@ -24,9 +25,51 @@ func main(){
 	fmt.Println(parsedTime)
 
 	t := time.Now()
+	fmt.Println("-------------------------------------------------------------")
 	fmt.Println("formatted time:", t.Format("Mon 02-01-06 15:04"))
 	var addedTime = t.Add(time.Hour * 24)
+
 	fmt.Println("one day later:", addedTime)
 	fmt.Println("formatted time:", addedTime.Format("Mon 02-01-06 15:04"))
 	fmt.Println("formatted time:", addedTime.Weekday())
+
+
+	fmt.Println("rounded time:", addedTime.Round(time.Hour))
+
+	//converting timezone into other
+	istLocation, err = time.LoadLocation("Asia/Kolkata")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var t1 = time.Date(2025,time.February,06,0,0,0,0,time.UTC)
+
+	var tLocal = t1.In(istLocation)
+	fmt.Println("-----------------------------------------------------------------")
+	fmt.Println("time (UTC) converted to local:",tLocal)
+
+	//it rounds off down time and will not go to next hour
+	fmt.Println("truncated time:",time.Now().Truncate(time.Hour))
+
+
+	//time in new york
+	yorkTime, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("time in new york:", time.Now().In(yorkTime))
+
+	fmt.Println("-------------------------------------------------------------")
+	//subtracted time
+	time1 := time.Date(2025, time.May, 26, 10, 0, 0, 0, time.UTC)
+	time2 := time.Date(2025, time.May, 26, 9, 30, 0, 0, time.UTC)
+
+	// Subtract t2 from t1
+	diff := time1.Sub(time2)
+
+	fmt.Println("sub time difference:", diff) 
+	fmt.Println("time 1 after time 2:", time1.After(time2)) 
+	
+
 }
