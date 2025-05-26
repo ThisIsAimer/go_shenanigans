211639cc528119f3926301234c43a926b2bc657e
raunak <raunakbiswas05@gmail.com>
2025-05-26 13:05:37 +0530

    formatting time + added time

--- intermediate/time/time.go
+++ intermediate/time/time.go
@@ -17,4 +17,16 @@ func main(){
 	//we could use time.UTC
 
 	fmt.Println("specific time:", specificTime)
+
+	//the initial time must be Mon jan 2 15:04:05 mst 2006
+	//turns string to time
+	var parsedTime,_ = time.Parse("2006-01-02","2020-06-09")
+	fmt.Println(parsedTime)
+
+	t := time.Now()
+	fmt.Println("formatted time:", t.Format("Mon 02-01-06 15:04"))
+	var addedTime = t.Add(time.Hour * 24)
+	fmt.Println("one day later:", addedTime)
+	fmt.Println("formatted time:", addedTime.Format("Mon 02-01-06 15:04"))
+	fmt.Println("formatted time:", addedTime.Weekday())
 }
