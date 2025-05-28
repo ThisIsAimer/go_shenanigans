85651307221e6507268a4915c249e5abdbdf22a7
raunak <raunakbiswas05@gmail.com>
2025-05-29 01:01:45 +0530

    whats buffer

---
+++ intermediate/bufio/bufio.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main(){
+	//buffering is reading or writing huge amounts of data in chunks
+	//its used in video processing, youtube and communication
+	fmt.Println()
+
+}
