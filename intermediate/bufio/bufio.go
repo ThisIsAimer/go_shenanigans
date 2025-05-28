9f2e7981b8286ab9986c86724e012cd880c77fb6
raunak <raunakbiswas05@gmail.com>
2025-05-29 01:15:04 +0530

    reading into byte slices

--- intermediate/bufio/bufio.go
+++ intermediate/bufio/bufio.go
@@ -1,9 +1,29 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
 
 func main(){
 	//buffering is reading or writing huge amounts of data in chunks
 	//its used in video processing, youtube and communication
+
+	//strings.new reader is a reader but not buffered
+	var reader = strings.NewReader("hello go bufferio package\n")
+	var bufReader = bufio.NewReader(reader)
+
+	//it will contain the read string
+	data := make([]byte,20)
 	fmt.Println()
+	//returns number of bytes it reads
+	n, err := bufReader.Read(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("the data read is %d and the data is: %s\n", n,data[:n])
 
 }
