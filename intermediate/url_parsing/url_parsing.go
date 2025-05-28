5007ba5f1bd3ed11523223c1f020ff204227707a
raunak <raunakbiswas05@gmail.com>
2025-05-28 11:17:28 +0530

    Url details

---
+++ intermediate/url_parsing/url_parsing.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// [scheme:][userinfo@][host][:port][/path][?quary][#fragment]
+	//scheme : http/https/ftp
+	//userinfo contains username and pass (optional)
+	//host - domain name or ip address
+	//port - port no. (op)
+	//path to the resource of the server
+	//quary- key value pairs
+	//fragment - specify a location within the resource (op)
+}
