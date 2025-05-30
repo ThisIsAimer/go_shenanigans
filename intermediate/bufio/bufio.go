4678ab0932fd01ebd582ce8f14c80ac465f9f22a
raunak <raunakbiswas05@gmail.com>
2025-05-30 13:45:56 +0530

    reader functionality. printing remaining string

--- intermediate/bufio/bufio.go
+++ intermediate/bufio/bufio.go
@@ -11,19 +11,30 @@ func main(){
 	//its used in video processing, youtube and communication
 
 	//strings.new reader is a reader but not buffered
-	var reader = strings.NewReader("hello go bufferio package\n")
-	var bufReader = bufio.NewReader(reader)
+	var strReader = strings.NewReader("hello go bufferio package\n")
+	var reader = bufio.NewReader(strReader)
 
 	//it will contain the read string
 	data := make([]byte,20)
 	fmt.Println()
 	//returns number of bytes it reads
-	n, err := bufReader.Read(data)
+	n, err := reader.Read(data)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Printf("the data read is %d and the data is: %s\n", n,data[:n])
+
+	line, err := reader.ReadString('\n')//must use single quotes
+
+	if err != nil {
+		fmt.Println("error is:",err)
+		return
+	}
+
+	//reader give out the remaining string that was left after transfering
+	//string into data.
+	println("string read is:", line)
 
 }
