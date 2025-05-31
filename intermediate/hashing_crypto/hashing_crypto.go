2e7cd1122c7d02d5e186ec494e2c82db943578ae
raunak <raunakbiswas05@gmail.com>
2025-05-31 12:02:05 +0530

    hashing incription in golang!

---
+++ intermediate/hashing_crypto/hashing_crypto.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+)
+
+func main() {
+	// hashing is a process in computing that transforms data
+	// into a fixed size string of characters
+	// it typically appears random
+	// implimented by a hash function
+	// if we use SHA-256 hash it makes a 256 bit or 32 byte long hash
+	// SHA-512 creates a 512 bit or 64 byte long hash
+	// hashing generates an ir-reversable output
+	// its hard to easily reverse an hash
+	// sha-256 can create 2^256 possible hash values
+
+	var password = "password1234"
+	var hashed = sha256.Sum256([]byte(password))
+	var hashed1 = sha512.Sum512([]byte(password))
+	fmt.Println("hashed value is:", hashed)
+
+	//must be printed with %x
+	fmt.Printf("hashed value is: %x\n", string(hashed[:]))
+	fmt.Printf("hashed value is: %x\n", string(hashed1[:]))
+
+}
