f88b544d41a98b78a4e8f286a1a6aa4ff41fc2e1
raunak <raunakbiswas05@gmail.com>
2025-05-31 21:46:45 +0530

    hashing passwards with salt. storing encripted salt value for more more security

--- intermediate/hashing_crypto/hashing_crypto.go
+++ intermediate/hashing_crypto/hashing_crypto.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"crypto/sha512"
+	"encoding/base64"
 	"fmt"
+	"io"
 )
 
 func main() {
@@ -26,4 +29,75 @@ func main() {
 	fmt.Printf("hashed sha-256 value is: %x\n", string(hashed[:]))
 	fmt.Printf("hashed sha-512 value is: %x\n", string(hashed1[:]))
 
+	fmt.Println("------------------------------------------------------")
+	fmt.Println("------------------------------------------------------")
+
+	salt,err := generateSalt()
+	if err != nil {
+		fmt.Println("error is:",err)
+		return
+	}
+	fmt.Printf("salt is: %x\n",salt)
+
+	var signupPass = hashPassward(password,salt)
+
+	encodedSalt := base64.StdEncoding.EncodeToString(salt)
+	fmt.Println("base 64 encoded salt:",encodedSalt) // stored to database
+	fmt.Println("Hash:",signupPass) // stored to database
+
+	fmt.Println("------------------------------------------------------")
+	fmt.Println("------------------------------------------------------")
+
+	//encoded salt is retrieved from database
+	decodedSalt,err := base64.StdEncoding.DecodeString(encodedSalt)
+	if err != nil {
+		fmt.Println("error is:",err)
+		return
+	}
+
+	fmt.Printf("base 64 decoded salt is:%x\n",decodedSalt)
+
+	loginPass := hashPassward(password,decodedSalt)
+
+	if loginPass == signupPass{
+		fmt.Println("logged in successfully!")
+	} else{
+		fmt.Println("login failed, please check username and password")
+	}
+
+
+	//what we are doing here is generating a salt and storing it to database after
+	//base64 encoding it.
+
+	//we make a new hash by passing the passward and the original salt value
+	//hashing it and then again base64 encoding it. we store it in database
+
+	//for login
+	//we get the base64 encoded salt then decoding it to get the original salt
+	
+	//after that we hash the passward with the original salt to get a hash that
+	//will be exactly same as the hash stord in the database if passward is same.
+
+	//then we compare both hashed values for login
+
+}
+
+
+
+func generateSalt() ([]byte,error){
+	salt := make([]byte,16)
+	_, err := io.ReadFull(rand.Reader,salt)
+	if err != nil {
+		return nil , err
+	} else{
+		return salt , nil
+	}	
+}
+
+
+func hashPassward(pass string, salt []byte) string {
+	saltedPassward := append(salt,[]byte(pass)...)
+
+	hash := sha256.Sum256(saltedPassward)
+	return base64.RawStdEncoding.EncodeToString(hash[:])
 }
