ed7b536be0eb150d2314309093862d36b4aebd31
raunak <raunakbiswas05@gmail.com>
2025-06-01 14:29:54 +0530

    using buffer, reading each line from file individually

--- intermediate/reading_file/reading_files.go
+++ intermediate/reading_file/reading_files.go
@@ -1,32 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
-func main(){
+func main() {
 
-	file, err :=os.Open("intermediate/reading_file/backend.txt")
+	file, err := os.Open("intermediate/reading_file/backend.txt")
 	if err != nil {
-		fmt.Println("error is:",err)
+		fmt.Println("error is:", err)
 		return
 	}
-	defer func(){
+	defer func() {
 		file.Close()
 		fmt.Println("file closed")
 	}()
 
-
-	var data = make([]byte,890)
+	var data = make([]byte, 895)
 
 	_, err = file.Read(data)
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("file content is:", string(data))
+
+	fmt.Println("-----------------------------------------------")
+
+	file1, err := os.Open("intermediate/reading_file/backend.txt")
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+	defer func() {
+		file1.Close()
+		fmt.Println("file1 closed")
+	}()
+
+	myBuffer := bufio.NewScanner(file1)
+
+	//scans each individual line of the file and puts it in my buffer
+	for myBuffer.Scan() {
+		line := myBuffer.Text()
+		//assigns the string read from file to line
+
+		fmt.Println(line)
+
+	}
+
+	err = myBuffer.Err()
 	if err != nil {
 		fmt.Println("error is:",err)
 		return
 	}
-
-
-	fmt.Println("file content is:", string(data))
 
 }
