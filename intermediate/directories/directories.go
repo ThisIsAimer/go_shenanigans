0604ad2080910c9266cf71f7a3aec47207fded79
raunak <raunakbiswas05@gmail.com>
2025-06-03 12:15:54 +0530

    reading directories with os.ReadDir and changing base directory with os.Chdi

--- intermediate/directories/directories.go
+++ intermediate/directories/directories.go
@@ -17,8 +17,28 @@ func main(){
 	err = os.MkdirAll("./intermediate/directories/c/a/b",0755)
 	checkError(err)
 
+	dir, err := os.ReadDir("./intermediate/directories")
+	checkError(err)
 
-	fmt.Println("hello world")
+	for _, element := range dir{
+		fmt.Println(element)
+		fmt.Println(element.IsDir())
+		fmt.Println("----------------------------------------------------------")
+	}
+
+	fmt.Println("----------------------------------------------------------")
+
+	//creates directory the base
+	checkError(os.Chdir("intermediate/directories"))
+	dir, err = os.ReadDir(".")
+	if err != nil {
+		fmt.Println("error is:",err)
+		return
+	}
+
+	fmt.Println("reading intermediate/directories: ")
+
+	fmt.Println(dir)
 
 }
 
