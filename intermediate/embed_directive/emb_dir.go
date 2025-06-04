a260f9ca14b294c7337fab75be33ffa68e7519fd
raunak <raunakbiswas05@gmail.com>
2025-06-05 00:05:27 +0530

    walking firectories with fs.WalkDir, reading embedded files with embDir.ReadFile

--- intermediate/embed_directive/emb_dir.go
+++ intermediate/embed_directive/emb_dir.go
@@ -1,16 +1,54 @@
 package main
-import (
-	"fmt"
-	_ "embed"
-)
 
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"log"
+)
 
 //must be in the same folder
 
 //go:embed example.txt
 var content string
 
-func main(){
-	fmt.Println("embeded content:",content)
+//go:embed golang
+var embDir embed.FS
+
+func main() {
+
+	fmt.Println("embeded content:", content)
+	
+	fileContent, err := embDir.ReadFile("golang/love.txt")
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+	fmt.Println("love.txt:", string(fileContent))
+
+	fileContent, err = embDir.ReadFile("golang/golang.txt")
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("golang.txt:\n", string(fileContent))
+	fmt.Println("-------------------------------------------------")
+
+	//this takes fs.FS unline filepath.walkdir that takes string
+	err = fs.WalkDir(embDir,"golang",func(path string, d fs.DirEntry, err error) error{
+		if err != nil {
+			fmt.Println("error is:",err)
+			return err
+		}
+
+		fmt.Println("file is:",path)
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 
 }
