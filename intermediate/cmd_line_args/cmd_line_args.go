8b07bd22d4946ae86cbf1ad1c936a652d43a9cc4
raunak <raunakbiswas05@gmail.com>
2025-06-05 12:14:29 +0530

    fixed indents with ctrl s

--- intermediate/cmd_line_args/cmd_line_args.go
+++ intermediate/cmd_line_args/cmd_line_args.go
@@ -6,12 +6,12 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 
 	fmt.Println("command:", os.Args[0])
 
 	//go run "e:\coding\golang\basics\intermediate\cmd_line_args\cmd_line_args.go" hello bro are are you
-	for index, args := range os.Args{
+	for index, args := range os.Args {
 		fmt.Printf("argument %d: %s\n", index, args)
 	}
 
@@ -23,13 +23,13 @@ func main(){
 	//if we pass male, it will be set to true!
 	var male bool
 
-	flag.StringVar(&name,"name","bro","your name")
-	flag.IntVar(&age,"age", 18, "your age")
-	flag.BoolVar(&male,"male",false,"are you male?")
+	flag.StringVar(&name, "name", "bro", "your name")
+	flag.IntVar(&age, "age", 18, "your age")
+	flag.BoolVar(&male, "male", false, "are you male?")
 
 	flag.Parse()
 
-	fmt.Println("name:",name)
+	fmt.Println("name:", name)
 	fmt.Println("age:", age)
 	fmt.Println("male?:", male)
 
