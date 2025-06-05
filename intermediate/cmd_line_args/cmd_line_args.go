960ff1ea13afb89b05ef7c3e9eb2043401ab6184
raunak <raunakbiswas05@gmail.com>
2025-06-05 12:03:07 +0530

    flags in golang command line interface with flag.StringVar, flag.IntVar flag.BoolVar and flag.Parse()

--- intermediate/cmd_line_args/cmd_line_args.go
+++ intermediate/cmd_line_args/cmd_line_args.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -13,5 +14,23 @@ func main(){
 	for index, args := range os.Args{
 		fmt.Printf("argument %d: %s\n", index, args)
 	}
+
+	//defining flags
+	//go run "e:\coding\golang\basics\intermediate\cmd_line_args\cmd_line_args.go" -name "androEz bro" -age 19 -male 1
+	////go run "e:\coding\golang\basics\intermediate\cmd_line_args\cmd_line_args.go" -name androEz -age 19
+	var name string
+	var age int
+	//if we pass male, it will be set to true!
+	var male bool
+
+	flag.StringVar(&name,"name","bro","your name")
+	flag.IntVar(&age,"age", 18, "your age")
+	flag.BoolVar(&male,"male",false,"are you male?")
+
+	flag.Parse()
+
+	fmt.Println("name:",name)
+	fmt.Println("age:", age)
+	fmt.Println("male?:", male)
 
 }
