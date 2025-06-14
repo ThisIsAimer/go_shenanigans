6ebab7538c87e65204b3194c9d2aad7bcf7a4bbf
raunak <raunakbiswas05@gmail.com>
2025-06-14 11:41:41 +0530

    proper working example of goroutine

--- advanced/goroutine/goroutines.go
+++ advanced/goroutine/goroutines.go
@@ -16,10 +16,16 @@ func sayHello() {
 func main(){
 	fmt.Println("hello world")
 
+	var variable string
+
 	//the programme ends faster then 2 secs
 	//so this code is never executed..
 	//if the programme was longer it would
 	//go processes it in  another thread
 	go sayHello()
+
+	fmt.Println("enter smth:")
+	fmt.Scanln(&variable)
+	fmt.Println("your variable is:", variable)
 
 }
