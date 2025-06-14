d8dfb1446d374eb104b1ee1db6e65df61c50f1d6
raunak <raunakbiswas05@gmail.com>
2025-06-14 11:37:35 +0530

    go routine explaination!

--- advanced/goroutine/goroutines.go
+++ advanced/goroutine/goroutines.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello() {
+	time.Sleep(2* time.Second)
+
+	fmt.Println("hello from goroutine!")
+
+
+}
 
 func main(){
 	fmt.Println("hello world")
+
+	//the programme ends faster then 2 secs
+	//so this code is never executed..
+	//if the programme was longer it would
+	//go processes it in  another thread
+	go sayHello()
 
 }
