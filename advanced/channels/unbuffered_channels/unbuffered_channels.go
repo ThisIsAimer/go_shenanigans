476a4d2c8c330f92ce7edd4f422624eb9a123ca5
raunak <raunakbiswas05@gmail.com>
2025-06-15 14:03:37 +0530

    Unbuffered channels machanism in golang

--- advanced/channels/unbuffered_channels/unbuffered_channels.go
+++ advanced/channels/unbuffered_channels/unbuffered_channels.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main(){
-	fmt.Println("hello world")
+	channel := make(chan string)//this makes a unbuffered channel
+
+	//only unbuffered channels need goroutines
+	go func(){
+		time.Sleep(2 * time.Second)
+		fmt.Println("2 seconds passed")
+
+		channel <- "Hey ther i am here"
+
+		time.Sleep(3 * time.Second)
+		fmt.Println("3 seconds passed")
+	}()
+
+	//an unbuffered channel will try to imidiately find a reciever of the value
+	//being result in this case
+	result := <-channel
+
+	//channels will only pause the programmes until they get a value. after it recieves the value the main
+	//programme will continue
+
+	fmt.Println(result)
 
 }
