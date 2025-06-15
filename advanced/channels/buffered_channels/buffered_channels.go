2941f4c8f5256a028e107e8f502bdd8cc6560577
raunak <raunakbiswas05@gmail.com>
2025-06-15 16:35:25 +0530

    channel buffer waiting to be empty to store more values

--- advanced/channels/buffered_channels/buffered_channels.go
+++ advanced/channels/buffered_channels/buffered_channels.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main(){
 
@@ -11,14 +14,26 @@ func main(){
 	//3 values will cause error
 	channel <- 7
 	channel <- 10
-	//channel <- 20
 
 	fmt.Println("this is buffered channel")
+
+	go func(){
+		time.Sleep(3 * time.Second)
+		result := <- channel
+		fmt.Println(result)
+
+	}()
+
+	//channel will wait for the channel to get empty and then execute
+	channel <- 20
+
 
 	result := <- channel
 	fmt.Println(result)
 
 	result = <- channel
 	fmt.Println(result)
+
+	fmt.Println("--------------------------------------------------------------")
 
 }
