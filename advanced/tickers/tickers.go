62b9694d0dc27d1085f386199b2bb2be01d6fea7
raunak <raunakbiswas05@gmail.com>
2025-06-17 17:59:23 +0530

    stopping ticker with timers time.After()

--- advanced/tickers/tickers.go
+++ advanced/tickers/tickers.go
@@ -15,10 +15,31 @@ func main(){
 		num++
 		fmt.Println(<- ticker.C)
 		fmt.Println("currently ticking:", num)
-		if num == 10{
+		if num == 4{
 			ticker.Stop()
 			break
 		}
 	}
 
+	fmt.Println("-----------------------------------------------------")
+
+	myTicker := time.NewTicker(time.Second)
+	stop := time.After(time.Second*5)
+
+
+	processing(myTicker,stop)
+
+}
+
+
+func processing(ticker *time.Ticker, timer <-chan time.Time){
+	for{
+		select{
+		case tick := <-ticker.C:
+			fmt.Println("processing:", tick)
+		case <-timer:
+			fmt.Println("timeout!")
+			return
+		}
+	}
 }
