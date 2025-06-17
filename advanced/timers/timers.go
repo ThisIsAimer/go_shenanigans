9f2af36ede05af6836e6b6a7f9344e90f9b4b171
raunak <raunakbiswas05@gmail.com>
2025-06-17 17:15:31 +0530

    timeout with time.after()

--- advanced/timers/timers.go
+++ advanced/timers/timers.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	timer := time.NewTimer(3 * time.Second)
+	timer := time.NewTimer(300 * time.Second)
 	fmt.Println("timer started!...")
 
 	time.Sleep(1 * time.Second)
@@ -18,13 +18,49 @@ func main() {
 	//stops timer manually
 	stopped = timer.Stop()
 
-	//<-timer.C is blocking in nature
 	if stopped {
 		fmt.Println("timer was stopped manually!")
-	} else {
-		<-timer.C
 	}
 
-	fmt.Println("end of programme")
+	fmt.Println("-------------------------------------------------------")
 
+	fmt.Println("reseted timer for 1 sec...")
+	timer.Reset(time.Second)
+
+	//<-timer.C is blocking in nature
+	<-timer.C
+
+	fmt.Println("end of timer")
+
+	fmt.Println("-------------------------------------------------------")
+
+
+	//time.after send process itself its like timer.c
+	process := time.After(3*time.Second)
+
+	done := make(chan bool)
+
+	go func(){ 
+		longProcess() 
+		done <- true
+		}()
+
+	select{
+	case <- process:
+		fmt.Println("process is timed out")
+
+	case <- done:
+		fmt.Println("long process done!")
+	}
+
+	fmt.Println("-------------------------------------------------------")
+
+}
+
+
+func longProcess(){
+	for i := range 20{
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
 }
