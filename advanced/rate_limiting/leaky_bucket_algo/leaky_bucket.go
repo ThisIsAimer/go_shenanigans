c2c14d3e9ece2344b70278c817c0b5b43a66bd5c
raunak <raunakbiswas05@gmail.com>
2025-06-21 14:14:13 +0530

    leaky bucket algo code

---
+++ advanced/rate_limiting/leaky_bucket_algo/leaky_bucket.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type leakyBucket struct{
+	mx sync.Mutex
+	capacity int
+	tokens int
+	leakRate time.Duration
+	lastLeak time.Time
+}
+
+func NewLeakyBucket(capacity int, leakRate time.Duration) *leakyBucket{
+	return &leakyBucket{
+		capacity: capacity,
+		tokens: capacity,
+		leakRate: leakRate,
+		lastLeak: time.Now(),
+	}
+}
+
+func (lb *leakyBucket) allow() bool {
+	lb.mx.Lock()
+	defer lb.mx.Unlock()
+
+	now := time.Now()
+
+	elapsedTime := now.Sub(lb.lastLeak)
+
+	tokensToAdd := int(elapsedTime/lb.leakRate)
+
+	if lb.tokens < lb.capacity{
+		lb.tokens += tokensToAdd
+	}
+
+	lb.lastLeak = lb.lastLeak.Add(time.Duration(tokensToAdd)* lb.leakRate)
+
+	if lb.tokens > 0{
+		lb.tokens--
+		return true
+	}
+
+	return false
+
+}
+
+
+func main(){
+
+	myLeakyBucket := NewLeakyBucket(5, time.Second)
+	
+	for range 20{
+		if myLeakyBucket.allow(){
+			fmt.Println("access was allowed")
+		}else {
+			fmt.Println("access denied")
+		}
+
+		time.Sleep(time.Second/4)
+	}
+
+}
