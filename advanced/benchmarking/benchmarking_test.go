89daec3991f3f5dc7285c6c27408acde51fe0425
raunak <raunakbiswas05@gmail.com>
2025-06-23 22:37:09 +0530

    testing with test table struct in go

--- advanced/benchmarking/benchmarking_test.go
+++ advanced/benchmarking/benchmarking_test.go
@@ -12,6 +12,24 @@ func add(a, b int) int {
 }
 
 
+//even in error it executes all arguments
+func TestAddTableDriven(t *testing.T){
+	tests := []struct{a,b,expected int}{
+		{2,3,5},
+		{2,5,8},
+		{5,3,8},
+		{2,8,9},
+	}
+
+	for _, test :=  range tests{
+		result := add(test.a,test.b)
+		if result != test.expected{
+			t.Errorf("add(%d,%d) = %d but we want %d",test.a,test.b,result,test.expected)
+		}
+	}
+}
+
+
 func TestAdd(t *testing.T){
 	result := add(2,3)
 	expected := 6
