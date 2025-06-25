b3c26c0531822124ba86ccd52b9ac956a98a1ed8
raunak <raunakbiswas05@gmail.com>
2025-06-25 19:00:06 +0530

    waiting with cmd exec.Command("sleep", "3"), cmd.Start() and cmd.Wait()

--- advanced/os_processes/process_spawning.go
+++ advanced/os_processes/process_spawning.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -18,5 +19,41 @@ func main() {
 	}
 
 	fmt.Println("output:", string(output))
+
+	//---------------------------------------------
+
+	cmd = exec.Command("grep", "love")
+
+	cmd.Stdin = strings.NewReader("a mother loves her child\nthanks for all the love and support\nloving\nokie")
+
+	output, err = cmd.Output()
+
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("output of reader:-")
+	fmt.Println(string(output))
+
+
+	fmt.Println("----------------------------------------------")
+
+	cmd = exec.Command("sleep", "3")
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	err = cmd.Wait()
+
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("after 3 secs")
 
 }
