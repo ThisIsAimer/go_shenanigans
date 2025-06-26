1e7718d6545e817266fc60ee3884cd639a8206d5
raunak <raunakbiswas05@gmail.com>
2025-06-26 11:20:00 +0530

    more exec command operations with io.pipe() and cmd.CombinedOutput()

--- advanced/os_processes/process_spawning.go
+++ advanced/os_processes/process_spawning.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
 	"time"
@@ -40,7 +41,7 @@ func main() {
 
 	fmt.Println("----------------------------------------------")
 
-	cmd = exec.Command("sleep", "10")
+	cmd = exec.Command("sleep", "2")
 
 	err = cmd.Start()
 	if err != nil {
@@ -50,7 +51,7 @@ func main() {
 
 	time.Sleep(time.Second * 2)
 
-	err = cmd.Process.Kill()
+	// err = cmd.Process.Kill()
 
 	fmt.Println("after 2 secs")
 
@@ -62,5 +63,53 @@ func main() {
 	}
 
 	fmt.Println("after 3 secs")
+
+	fmt.Println("-----------------------------------")
+
+	cmd = exec.Command("printenv","GOPATH")
+
+	output,err = cmd.Output()
+
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("env:", string(output))
+
+	fmt.Println("-----------------------------------")
+
+
+	pr, pw := io.Pipe()
+
+	cmd = exec.Command("grep", "love")
+
+	cmd.Stdin = pr
+
+	go func(){
+		defer pw.Close()
+		pw.Write([]byte("a mother loves her child\nthanks for all the love and support\nloving\nokie"))
+	}()
+
+	output ,err = cmd.Output()
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("the output of pipe is :-")
+	fmt.Println(string(output))
+
+	fmt.Println("-----------------------------------------")
+	cmd = exec.Command("ls","-l")
+
+	output ,err = cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	fmt.Println("ls:", string(output))
 
 }
