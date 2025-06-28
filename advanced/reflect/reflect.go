f88501dd7b90e44504fd35c3584efb6f07894bb4
raunak <raunakbiswas05@gmail.com>
2025-06-28 14:37:17 +0530

    reflecting in go with v1.Type(), reflect.Kind(), y.IsZero() and v1.SetInt()

--- advanced/reflect/reflect.go
+++ advanced/reflect/reflect.go
@@ -12,6 +12,39 @@ func main(){
 	y := reflect.ValueOf(x)
 	v := y.Type()
 
-	fmt.Println("Value type:", v)
+	fmt.Println("Value x type:", v)
+	fmt.Println("Value x Kind:", v.Kind())
+	fmt.Println("Value x is int?:", v.Kind() == reflect.Int)
+	fmt.Println("Value x is string?:", v.Kind() == reflect.String)
+	fmt.Println("is x zero?:", y.IsZero())
+
+	fmt.Println("-----------------------------------------------")
+
+	a := 70
+
+	v1 := reflect.ValueOf(&a).Elem()
+	v2 := reflect.ValueOf(&a)
+	fmt.Println("v1 is type:", v1.Type())
+	fmt.Println("v2 is type:", v2.Type())
+
+	fmt.Println("v1 int value is:", v1.Int())
+	v1.SetInt(20)
+	fmt.Println("now, v1 int value is:", v1.Int())
+
+	fmt.Println("-----------------------------------------------")
+
+	var itf any = "Golang"
+
+	myReflect := reflect.ValueOf(itf)
+
+	fmt.Println("type of itf:", myReflect.Type())
+
+	if( myReflect.Kind() == reflect.String){
+		fmt.Println("itf is of string type")
+	}
+	
+
+	
+
 
 }
