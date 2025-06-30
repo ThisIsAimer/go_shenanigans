df1fd85d302f63efc030c593d64a8bf4ec907f5d
raunak <raunakbiswas05@gmail.com>
2025-06-30 06:47:11 +0530

    race conditions testing command and prevention with mutexes

--- after_advanced/race_conditions/race_conditons.go
+++ after_advanced/race_conditions/race_conditons.go
@@ -1,8 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+we commented lines  21/22 of  function incriment to simulate data races!
+we can test for data races with the go command
+go run -race <FileName/FilePath>
+to test for race conditions
+*/
+
+type counter struct{
+	mutex sync.Mutex
+	count int
+}
+
+func (c *counter) incriment(){
+	// c.mutex.Lock()
+	// defer c.mutex.Unlock()
+
+	c.count++
+}
+
+func (c *counter) getValue() int {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.count
+}
+
+
 
 func main(){
-	fmt.Println("hello world")
+	// mutexes disallow other go routines to temper with already running functions
+
+	var wg sync.WaitGroup
+
+	myCounter := &counter{}
+
+	numGoroutines := 10
+
+
+	for range numGoroutines{
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			for range 1000{
+				myCounter.incriment()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("the final counter is", myCounter.getValue())
+
+	fmt.Println("---------------------------------------------------------------")
+	 var counter int = 0
+	 var wait sync.WaitGroup
+	 var mutex sync.Mutex
+
+	 numGoroutines = 5
+
+	 wait.Add(numGoroutines)
+
+	 incriment := func(){
+
+		defer wait.Done()
+
+		for range 1000{
+			mutex.Lock()
+			counter++
+			mutex.Unlock()
+		}
+	 }
+
+	 for range numGoroutines{
+	 	go incriment()
+	 }
+	 wait.Wait()
+
+	 fmt.Println("the counter is now:", counter)
+
 
 }
