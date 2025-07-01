aa21e038dda13b5b982d8fcc3c2523c5c9a8d1b1
raunak <raunakbiswas05@gmail.com>
2025-07-01 11:26:34 +0530

    sync.Cond wait conditions in go with example

--- after_advanced/new_cond/new_cond.go
+++ after_advanced/new_cond/new_cond.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
-
 
 const bufferSize = 5
 
-type buffer struct{
+type buffer struct {
 	items []int
-	mux sync.Mutex
-	cond *sync.Cond
+	mux   sync.Mutex
+	cond  *sync.Cond
 }
 
-
-func newBuffer(size int) *buffer{
+func newBuffer(size int) *buffer {
 	b := &buffer{
 		items: make([]int, 0, size),
 	}
@@ -24,36 +23,61 @@ func newBuffer(size int) *buffer{
 	return b
 }
 
-func (b *buffer) produce(item int){
+func (b *buffer) produce(item int) {
 	b.mux.Lock()
 	defer b.mux.Unlock()
 
-	for len(b.items) >= bufferSize{
+	for len(b.items) >= bufferSize {
+		// what it does is it unloacks and locks the function continiously so that
+		// other go routines can access the elements and modify them
 		b.cond.Wait()
 	}
 
 	b.items = append(b.items, item)
-	fmt.Println("produced a new item:", item)
+	fmt.Println("produced a  new item:", item)
 	b.cond.Signal()
-	
+
 }
 
-func (b *buffer) consume() int{
+func (b *buffer) consume() int {
 	b.mux.Lock()
 	defer b.mux.Unlock()
 
-	for len(b.items)  == 0 {
+	for len(b.items) == 0 {
 		// if 0 it stops and waits for other func to append to slice
 		b.cond.Wait()
 	}
 	item := b.items[0]
 	b.items = b.items[1:]
-	fmt.Println("consumed:",item)
+	fmt.Println("consumed:", item)
 	b.cond.Signal()
 	return item
 }
 
-func main(){
-	fmt.Println("new programe")
+func producer(buf *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range 10 {
+		buf.produce(i + 100)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
 
+func consumer(buf *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for range 10 {
+		buf.consume()
+		time.Sleep(time.Millisecond * 500)
+	}
+
+}
+
+func main() {
+	buffer := newBuffer(bufferSize)
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go producer(buffer, &wg)
+	go consumer(buffer, &wg)
+
+	wg.Wait()
 }
