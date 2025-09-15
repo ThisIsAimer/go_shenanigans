bea7372f37db2ef2b10bf718fe492196d1d124ad
raunak <raunakbiswas05@gmail.com>
2025-09-15 12:34:37 +0530

    setting up client

--- grpc_and_protocal_buffers/grpc_client_example/client.go
+++ grpc_and_protocal_buffers/grpc_client_example/client.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	port := ":50051"
+
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	defer conn.Close()
+
+}
